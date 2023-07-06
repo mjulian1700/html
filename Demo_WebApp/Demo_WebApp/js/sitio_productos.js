18143b96b36a305329255ac8f4b10dc65bc02472
eduit-learning <achavez@grupoeduit.com>
2023-07-06 13:18:59 -0600

    ejercicio

--- Demo_WebApp/Demo_WebApp/js/sitio_productos.js
+++ Demo_WebApp/Demo_WebApp/js/sitio_productos.js
@@ -6,19 +6,40 @@ function productos_loadStaticContent() {
     let productos = [
         {
             nombre: "Fresa", descripcion: "Fresa deliciosa de Guanajuato",
-            precioKg: 100.00, stockKg: 100
+            precioKg: 100.00, stockKg: 100,
+            imgUri: '../media/images/img_fresa.jpg'
         },
         {
             nombre: "Mango", descripcion: "Mango delicioso y petacón de Veracrúz",
-            precioKg: 40.00, stockKg: 400
+            precioKg: 40.00, stockKg: 400,
+            imgUri: '../media/images/img_mango.jpg'
         },
         {
             nombre: "Uva", descripcion: "Uva verde deliciosa sin semilla de Baja California",
-            precioKg: 80.00, stockKg: 200
+            precioKg: 80.00, stockKg: 200,
+            imgUri: '../media/images/img_uva.jpg'
         },
         {
             nombre: "Melón", descripcion: "Melón chino delicioso de Torreón Coahuila",
-            precioKg: 20.00, stockKg: 800
+            precioKg: 20.00, stockKg: 800,
+            imgUri: '../media/images/img_melon.jpg'
+        },
+        {
+            nombre: "Pera", descripcion: "Pera deliciosa para los chiles en nogada de Puebla",
+            precioKg: 60.00, stockKg: 1000,
+            imgUri: '../media/images/img_pera.jpg'
         }
     ]
+
+    const catalogSection = document.getElementById('productos-catalogo-frutas')
+    let articles = ''
+    for (let item of productos) {
+        articles += '<article>'
+        articles += '<h2>' + item.nombre + '</h2>'
+        articles += "<img src='" + item.imgUri + "'/>"
+        articles += '<p>' + item.descripcion + '</p>'
+        articles += '<p>$' + item.precioKg + '</p>'
+        articles += '</article>'
+    }
+    catalogSection.innerHTML = articles
 }
