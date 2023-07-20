68f9436959641d9a775d26cfbdf2c898706b1b7e
eduit-learning <achavez@grupoeduit.com>
2023-07-20 13:05:51 -0600

    Ejercicio en clase

---
+++ Demo_WebApp/Demo_WebApp/js/sitio_contacto.js
@@ -0,0 +1,20 @@
+﻿function contacto_initialLoad() {
+
+}
+
+function contacto_validarFormulario() {
+    //Solución 1
+    //if (document.querySelector('#contacto-inp-propridad').value == 0) {
+    //    alert('Selecciona algo')
+    //    return false
+    //}
+
+    //Solución 2
+    console.log(document.forms)
+    if (document.forms['contacto-formulario']['Prioridad'].value
+== 0    ) {
+        alert('Selecciona algo')
+        return false
+    }
+    return true
+}
