d71d7aa45ad949904dbb932e0183120e4081722a
eduit-learning <achavez@grupoeduit.com>
2023-07-24 11:43:27 -0600

    ejercicio

--- Demo_WebApp/Demo_WebApp/js/sitio_goblal.js
+++ Demo_WebApp/Demo_WebApp/js/sitio_goblal.js
@@ -15,39 +15,43 @@ function link_anchorWithHtmlPages(event, objectoEdicion) {
             location.reload()
             break
         case 'productos-page':
-            getHtmlContent('../sitio/productos.html', objectoEdicion)
+            getHtmlContent('../sitio/productos.html', 'GET',
+                mostrarContenidoHtml)
             break
         case 'pedidos-page':
-            getHtmlContent('../sitio/pedidos.html')
+            getHtmlContent('../sitio/pedidos.html', 'GET',
+                mostrarContenidoHtml)
             break
         case 'contacto-page':
-            getHtmlContent('../sitio/contacto.html')
+            getHtmlContent('../sitio/contacto.html', 'GET',
+                mostrarContenidoHtml)
             break
     }
 }
 
-function getHtmlContent(url, objectoEdicion) {
+function mostrarContenidoHtml() {
     const divContent = document.getElementById('html-dynamic-content')
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', url, true)
-    xhr.setRequestHeader('Cache-Control',
-        'no-cache, no-store, max-age=0')
-    xhr.setRequestHeader('Expires', 'Thu, 1 Jan 1970 00:00:00 GMT')
-    xhr.setRequestHeader('Pragma', 'no-cache')
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            divContent.innerHTML = this.responseText
-            if (url === '../sitio/productos.html') {
-                productos_initialLoad(objectoEdicion)
-            } else if (url === '../sitio/pedidos.html') {
-                pedidos_initialLoad()
-            }
-            else if (url === '../sitio/contacto.html') {
-                contacto_initialLoad()
-            }
+    if (this.readyState == 4 && this.status == 200) {
+        divContent.innerHTML = this.responseText
+        if (url === '../sitio/productos.html') {
+            productos_initialLoad(objectoEdicion)
+        } else if (url === '../sitio/pedidos.html') {
+            pedidos_initialLoad()
+        }
+        else if (url === '../sitio/contacto.html') {
+            contacto_initialLoad()
         }
     }
+}
 
+function getHtmlContent(url, metodo, callbackSuccess, callbackError) {
+    let xhr = new XMLHttpRequest()
+    xhr.open(metodo, url, true)
+    xhr.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0')
+    xhr.setRequestHeader('Expires', 'Thu, 1 Jan 1970 00:00:00 GMT')
+    xhr.setRequestHeader('Pragma', 'no-cache')
+    xhr.onerror = callbackError
+    xhr.onreadystatechange = callbackSuccess
     xhr.send()
 }
 
